1baf317464f5633858dac3ae1f5d636b0efac3dc
Youssif Al-Halawche <yhalawche@gmail.com>
2026-04-20 23:05:39 -0600

    disable jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({ 
+  site: 'https://tygrisiq.github.io/Youssif-Halawche/'
+  , base: '/Youssif-Halawche'
+})
